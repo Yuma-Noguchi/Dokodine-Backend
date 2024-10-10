fc89b1036f86ed081ccc752962f02b197a04e2f3
YumaNoguchi39 <n.yuma39@icloud.com>
2024-10-10 10:46:07 +0100

    modified workflow for dev and prod

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,21 @@
 variable "project_id" {
   description = "The ID of the Google Cloud project"
+  type = string
   default = "dokodine"
 }
 
 variable "region" {
   description = "The region to deploy the Cloud Run service"
+  type = string
   default = "us-central1"
 }
 
 variable "service_name" {
   description = "The name of the Cloud Run service"
-  default = "dokodine-backend"
+  type = string
 }
 
 variable "repository_id" {
   description = "The ID of the Artifact Registry repository"
-  default = "dokodine-backend-repo"
+  type = string
 }
